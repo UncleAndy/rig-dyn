1eb629511326457b3b497592fe82c7a62016ecbe
Andrey Velikoredchanin <andy@andyhost.ru>
2026-05-06 13:24:41 +0200

    Small fix

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,9 @@ mod traits;
 pub use client::Client;
 pub use provider::Provider;
 pub use client::RigClientCompletionModelAdapter;
-pub use traits::{CompletionModel, DynEmbeddingModel, RigCompletionModelAdapter};
+pub use traits::{
+    CompletionModel,
+    DynEmbeddingModel,
+    RigCompletionModelAdapter,
+    RigEmbeddingModelAdapter,
+};
